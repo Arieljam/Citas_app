9466f59ed84e124ae863900c33e7a8c793c26632
Arieljam <arieljam.cba@gmail.com>
2025-10-01 16:47:44 -0300

    Create main.dart

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'screens/login_screen.dart';
+
+void main() {
+  runApp(CitasApp());
+}
+
+class CitasApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Citas App',
+      theme: ThemeData(
+        primarySwatch: Colors.pink,
+        scaffoldBackgroundColor: Colors.white,
+      ),
+      debugShowCheckedModeBanner: false,
+      home: LoginScreen(),
+    );
+  }
+}
